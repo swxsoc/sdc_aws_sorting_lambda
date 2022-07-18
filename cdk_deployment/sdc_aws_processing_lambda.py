f35a3ca616465b932f31448cb9820769aa84139d
Damian Barrous-Dume <damianbarrous@gmail.com>
2022-07-18 11:38:25 +0100

    Edit requirements

--- cdk_deployment/sdc_aws_processing_lambda.py
+++ cdk_deployment/sdc_aws_processing_lambda.py
@@ -11,7 +11,7 @@ class SDCAWSSortingLambdaStack(Stack):
         sdc_aws_sorting_function = aws_lambda.Function(
             scope=self,
             id="aws_sdc_sorting_function",
-            handler="lambda.handler",
+            handler="lambda_function.handler",
             runtime=aws_lambda.Runtime.PYTHON_3_9,
             memory_size=128,
             function_name="aws_sdc_sorting_function",
