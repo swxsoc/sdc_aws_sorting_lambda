bc58d97067ba4d51647afe5b5a43debb134d8193
Damian Barrous-Dume <damianbarrous@gmail.com>
2022-07-25 13:13:32 +0100

    Add sorting function logic

--- lambda_function/file_sorter/file_sorter.py
+++ lambda_function/file_sorter/file_sorter.py
@@ -6,13 +6,18 @@ TODO: Skeleton Code for initial repo, class still needs to be implemented includ
 logging to DynamoDB + S3 log file and docstrings expanded
 """
 
-import logging
+from hermes_core import log
+from hermes_core.util import util
 import boto3
+import botocore
 
-logger = logging.getLogger()
-# Debug mode used for development
-# logger.setLevel(logging.DEBUG)
 
+INSTRUMENT_BUCKET_NAMES = {
+    "eea": "hermes-eea", 
+    "nemesis": "hermes-nemsis",
+    "merit": "hermes-merit",
+    "spani": "hermes-spani"
+}
 
 class FileSorter:
     """
@@ -20,124 +25,83 @@ class FileSorter:
     bucket event which triggered the lambda function to be called.
     """
 
-    def __init__(self, file_key):
+    def __init__(self, s3_bucket, s3_object):
         """
-        FileSorter Constructor
+        FileSorter Constructorlogger
         """
 
         # Initialize Class Variables
-        self.bucket = "swsoc-incoming"
-        self.file_key = file_key
+        try:
+            self.incoming_bucket_name = s3_bucket["name"]
+            log.info(f"Incoming Bucket Name Parsed Successfully: {self.incoming_bucket_name}")
 
-    def sort_file(self):
+        except KeyError:
+            error_message = "KeyError when extracting S3 Bucket Name/ARN from dict"
+            log.error(error_message)
+            raise KeyError(error_message)
+
+        try:
+            self.file_key = s3_object["key"]
+            self.file_etag = s3_object["eTag"]
+
+            log.info(f"Incoming Object Name Parsed Successfully: {self.file_key}")
+            log.info(f"Incoming Object eTag Parsed Successfully: {self.file_etag}")
+
+        except KeyError:
+            error_message = "KeyError when extracting S3 Object Name/eTag from dict"
+            log.error(error_message)
+            raise KeyError(error_message)
+
+        # Call sort function
+        self._sort_file()
+
+    def _sort_file(self):
         """
         Function that chooses calls correct sorting function
         based off file key name.
         """
 
-        if "spani" in self.file_key:
-            self._sort_spani_file()
+        # Dict of parsed science file
+        self.destination_bucket = self._get_destination_bucket()
 
-        elif "nemesis" in self.file_key:
-            self._sort_nemesis_file()
+        # Copy file to destination bucket
+        self._copy_from_incoming_to_destination()
 
-        elif "eea" in self.file_key:
-            self._sort_eea_file()
+    def _get_destination_bucket(self):
+        """
+        Returns bucket in which the file will be sorted to
+        """
+        try:
+            science_file = util.parse_science_filename(self.file_key)
+            destination_bucket = INSTRUMENT_BUCKET_NAMES[science_file["instrument"]]
+            log.info(f"Destination Bucket Parsed Successfully: {destination_bucket}")
+            
+            return destination_bucket
 
-        elif "merit" in self.file_key:
-            self._sort_merit_file()
+        except ValueError as e:
+            log.error(e)
 
-        else:
-            logger.error("ERROR: Not a valid file key")
-            raise Exception("Not a valid file key event")
+            raise ValueError(e)
 
-    def _copy_from_incoming_to_bucket(self, destination_bucket):
+    def _copy_from_incoming_to_destination(self):
 
         """
         Function to copy file from S3 incoming bucket using bucket key
         to destination bucket
         """
-        logger.info("Moving File: %s", self.file_key)
+        log.info(f"Initiating Copying of File From {self.incoming_bucket_name} to {self.destination_bucket}")
+
         try:
-            # TODO: Add downloading logic
+            # Initialize S3 Client and Copy Source Dict
             s3 = boto3.resource("s3")
-            copy_source = {"Bucket": self.bucket, "Key": self.file_key}
+            copy_source = {"Bucket": self.incoming_bucket_name, "Key": self.file_key}
+
             # Copy S3 file from incoming bucket to destination bucket
-            s3.meta.client.copy(copy_source, destination_bucket, self.file_key)
-            logger.info("File Moved Successfully: %s", self.file_key)
+            s3.meta.client.copy(copy_source, self.destination_bucket, self.file_key)
+            log.info(f"File {self.file_key} Successfully Moved to {self.destination_bucket}")
 
-            # Remove file from incoming bucket
-            logger.info("Removing File From Incoming Bucket: %s", self.file_key)
-            # obj = s3.Object(self.bucket, self.file_key)
-            # obj.delete()
-            logger.info("File Removed Successfully: %s", self.file_key)
-            return True
+        except botocore.exceptions.ClientError as e:
+            log.error(e)
 
-        except BaseException as e:
-            logger.error("Error when Sorting File: %s", e)
+            raise e
 
-    def _log_status_in_dynamo_db(self):
-        """
-        This function logs the if the file was sorted successfully/unsuccessfully
-        in a DynamoDB table
-        TODO: Add logging logic
-        """
-
-    def _log_status_in_s3(self):
-        """
-        This function logs the if the file was sorted successfully/unsuccessfully
-        in a logfile in an S3 Bucket
-        TODO: Add logging logic
-        """
-
-    def _sort_spani_file(self):
-        """
-        This function sorts file into the SPAN-i Instrument Bucket
-        TODO: Add sorting logic
-        """
-        logger.info("Sorting SPAN-i Instrument File: %s", self.file_key)
-        try:
-            # self._copy_from_incoming_to_bucket("hermes-spani")
-            logger.info("File Sorted Successfully: %s", self.file_key)
-
-        except BaseException as e:
-            logger.error("Error when Sorting File: %s", e)
-
-    def _sort_nemesis_file(self):
-        """
-        This function sorts file into the NEMESIS Instrument Bucket
-        TODO: Add processing logic
-        """
-        logger.info("Sorting NEMESIS Instrument File: %s", self.file_key)
-        try:
-            # self._copy_from_incoming_to_bucket("hermes-nemesis")
-            logger.info("File Sorted Successfully: %s", self.file_key)
-
-        except BaseException as e:
-            logger.error("Error when Sorting File: %s", e)
-
-    def _sort_eea_file(self):
-        """
-        This function sorts file into the EEA Instrument Bucket
-        TODO: Add sorting logic
-        """
-        logger.info("Sorting EEA Instrument File: %s", self.file_key)
-        try:
-            # self._copy_from_incoming_to_bucket("hermes-eea")
-            logger.info("File Sorted Successfully: %s", self.file_key)
-
-        except BaseException as e:
-            logger.error("Error when Sorting File: %s", e)
-
-    def _sort_merit_file(self):
-        """
-        This function sorts file into the MERIT Instrument Bucket
-        TODO: Add sorting logic
-        """
-        logger.info("Sorting MERIT Instrument File: %s", self.file_key)
-        try:
-            # self._copy_from_incoming_to_bucket("hermes-merit")
-            logger.info("File Sorted Successfully: %s", self.file_key)
-
-        except BaseException as e:
-            logger.error("Error when Sorting File: %s", e)
--- lambda_function/lambda_function.py
+++ lambda_function/lambda_function.py
@@ -23,36 +23,43 @@ def handler(event, context):
     environment.
     """
     # Extract needed information from event
-    print(event)
     try:
-        file_key = event["FileKey"]
-        environment = os.getenv("LAMBDA_ENVIRONMENT")
-        # Pass required variables to sort function and returns a 200 (Successful)
-        # / 500 (Error) HTTP response
-        response = sort_file(environment, file_key)
+        for s3_event in event['Records']:
+                
+            s3_bucket = s3_event["s3"]["bucket"]
+            s3_object = s3_event["s3"]["object"]
 
-        return response
+            print(s3_bucket)
+            print(s3_object)
 
-    except BaseException as e:
+            # environment = os.getenv("LAMBDA_ENVIRONMENT")
+            environment = "PRODUCTION"
+            # Pass required variables to sort function and returns a 200 (Successful)
+            # / 500 (Error) HTTP response
+            response = sort_file(environment, s3_bucket, s3_object)
+
+            return response
+
+    except KeyError:
+        
         return {
             "statusCode": 500,
-            "body": json.dumps(f"Error Extracting Variables from Event: {e}"),
+            "body": json.dumps(f"Key Error Extracting Variables from Event"),
         }
 
 
-def sort_file(environment, file_key):
+def sort_file(environment, s3_bucket, s3_object):
     """
     This is the main function that handles logic that initializes the
     FileProcessor class in it's correct environment.
     """
 
     # Production (Official Release) Environment / Local Development
-    if environment == "PRODUCTION" and "dev_" not in file_key:
+    if environment == "PRODUCTION":
         try:
             logger.info("Initializing FileSorter - Environment: Production")
-            sorter = FileSorter(file_key)
+            sorter = FileSorter(s3_bucket=s3_bucket, s3_object=s3_object)
             logger.warning("FileSorter Initialized Successfully")
-            sorter.sort_file()
 
             return {"statusCode": 200, "body": json.dumps("File Sorted Successfully")}
 
@@ -62,10 +69,10 @@ def sort_file(environment, file_key):
             return {"statusCode": 500, "body": json.dumps("Error Sorting File")}
 
     # Development (Master Branch but not Official Release) Environment
-    elif environment == "DEVELOPMENT" and "dev_" in file_key:
+    elif environment == "DEVELOPMENT":
         try:
             logger.info("Initializing FileSorter - Environment: Development")
-            sorter = FileSorter(file_key)
+            sorter = FileSorter(s3_bucket=s3_bucket, s3_object=s3_object, dry_run=True)
             logger.warning("FileSorter Initialized Successfully")
             sorter.sort_file()
 
