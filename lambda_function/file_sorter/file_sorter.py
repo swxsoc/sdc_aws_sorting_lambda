ea0da6cc2019cae0ec6032b333211fdc0909f065
Damian Barrous-Dume <damianbarrous@gmail.com>
2022-08-17 15:42:23 +0100

    Make class function more general

--- lambda_function/file_sorter/file_sorter.py
+++ lambda_function/file_sorter/file_sorter.py
@@ -76,19 +76,32 @@ class FileSorter:
         based off file key name.
         """
         # Verify object exists in incoming bucket
-        if self._verify_object_exists(self.incoming_bucket_name) or self.dry_run:
+        if (
+            self._verify_object_exists(
+                bucket=self.incoming_bucket_name,
+                file_key=self.file_key,
+                etag=self.file_etag,
+            )
+            or self.dry_run
+        ):
 
             # Dict of parsed science file
-            self.destination_bucket = self._get_destination_bucket()
+            self.destination_bucket = self._get_destination_bucket(
+                file_key=self.file_key
+            )
 
+            # Verify object does not exist in destination bucket
             if not self._verify_object_exists(
-                self.destination_bucket, check_etag=False
+                bucket=self.destination_bucket, file_key=self.file_key
             ):
                 # Copy file to destination bucket
-                self._copy_from_incoming_to_destination()
+                self._copy_from_source_to_destination(
+                    source_bucket=self.incoming_bucket_name,
+                    file_key=self.file_key,
+                    destination_bucket=self.destination_bucket,
+                )
             else:
                 # Add to unsorted if object already exists in destination bucket
-                self.destination_bucket = UNSORTED_BUCKET_NAME
                 self.file_key = (
                     f"{self.file_key}_"
                     f"{datetime.datetime.utcnow().strftime('%Y-%m-%d-%H%MZ')}"
@@ -98,28 +111,43 @@ class FileSorter:
                     "moving to unsorted bucket"
                 )
                 # Copy file to unsorted bucket
-                self._copy_from_incoming_to_destination()
+                self._copy_from_source_to_destination(
+                    source_bucket=self.incoming_bucket_name,
+                    file_key=self.file_key,
+                    destination_bucket=UNSORTED_BUCKET_NAME,
+                )
 
                 # remove file from incoming bucket
-                self._remove_object_from_incoming()
+                self._remove_object_from_bucket(
+                    bucket=self.incoming_bucket_name, file_key=self.file_key
+                )
 
             # Verify object exists in destination bucket
             # before removing it from incoming (Unless Dry Run)
-            if self._verify_object_exists(self.destination_bucket) or self.dry_run:
+            if (
+                self._verify_object_exists(
+                    bucket=self.destination_bucket,
+                    file_key=self.file_key,
+                    etag=self.file_etag,
+                )
+                or self.dry_run
+            ):
 
                 # Remove object from incoming bucket
-                self._remove_object_from_incoming()
+                self._remove_object_from_bucket(
+                    bucket=self.incoming_bucket_name, file_key=self.file_key
+                )
 
         else:
             raise ValueError("File does not exist in bucket")
 
-    def _get_destination_bucket(self):
+    def _get_destination_bucket(self, file_key):
         """
         Returns bucket in which the file will be sorted to
         """
         try:
 
-            science_file = util.parse_science_filename(self.file_key)
+            science_file = util.parse_science_filename(file_key)
 
             destination_bucket = INSTRUMENT_BUCKET_NAMES[science_file["instrument"]]
             log.info(f"Destination Bucket Parsed Successfully: {destination_bucket}")
@@ -131,69 +159,74 @@ class FileSorter:
 
             raise ValueError(e)
 
-    def _verify_object_exists(self, bucket, check_etag=True):
+    def _verify_object_exists(self, bucket, file_key, etag=None):
         """
         Returns wether or not the file exists in the specified bucket
         """
         try:
             s3 = boto3.resource("s3")
-            s3_bucket_object = s3.ObjectSummary(bucket, self.file_key)
+            s3_bucket_object = s3.ObjectSummary(bucket, file_key)
 
             # Checks to see that both the file key the same
-            if s3_bucket_object.key == self.file_key:
+            if s3_bucket_object.key == file_key:
                 # Checks to see if the file eTag is the same if check_etag is True
-                if check_etag:
-                    if s3_bucket_object.e_tag == self.file_etag:
-                        log.info(f"File {self.file_key} exists in Bucket {bucket}")
+                if etag:
+                    if s3_bucket_object.e_tag == etag:
+                        log.info(f"File {file_key} exists in Bucket {bucket}")
                         return True
                     else:
                         return False
                 else:
                     return True
             else:
-                log.info(f"File {self.file_key} does not exist in Bucket {bucket}")
+                log.info(f"File {file_key} does not exist in Bucket {bucket}")
 
                 return False
 
         except botocore.exceptions.ClientError:
-            log.info(f"File {self.file_key} does not exist in Bucket {bucket}")
+            log.info(f"File {file_key} does not exist in Bucket {bucket}")
 
             return False
 
-    def _copy_from_incoming_to_destination(self):
+    def _copy_from_source_to_destination(
+        self,
+        source_bucket=None,
+        destination_bucket=None,
+        file_key=None,
+        new_file_key=None,
+    ):
 
         """
         Function to copy file from S3 incoming bucket using bucket key
         to destination bucket
         """
-        log.info(
-            f"Copying From {self.incoming_bucket_name} to {self.destination_bucket}"
-        )
+        log.info(f"Copying {file_key} From {source_bucket} to {destination_bucket}")
 
         try:
             # Initialize S3 Client and Copy Source Dict
             s3 = boto3.resource("s3")
-            copy_source = {"Bucket": self.incoming_bucket_name, "Key": self.file_key}
+            copy_source = {"Bucket": source_bucket, "Key": file_key}
 
             # Copy S3 file from incoming bucket to destination bucket
             if not self.dry_run:
-                bucket = s3.Bucket(self.destination_bucket)
-                bucket.copy(copy_source, self.file_key)
-            log.info(
-                f"File {self.file_key} Successfully Moved to {self.destination_bucket}"
-            )
+                bucket = s3.Bucket(destination_bucket)
+                if new_file_key:
+                    bucket.copy(copy_source, new_file_key)
+                else:
+                    bucket.copy(copy_source, file_key)
+            log.info(f"File {file_key} Successfully Moved to {destination_bucket}")
 
         except botocore.exceptions.ClientError as e:
             log.error(e)
 
             raise e
 
-    def _remove_object_from_incoming(self):
+    def _remove_object_from_bucket(self, bucket, file_key):
         """
         Function to copy file from S3 incoming bucket using bucket key
         to destination bucket
         """
-        log.info(f"Removing From {self.file_key} from {self.incoming_bucket_name}")
+        log.info(f"Removing From {file_key} from {bucket}")
 
         try:
             # Initialize S3 Client and Copy Source Dict
@@ -201,14 +234,9 @@ class FileSorter:
 
             # Copy S3 file from incoming bucket to destination bucket
             if not self.dry_run:
-                s3.Object(self.incoming_bucket_name, self.file_key).delete()
+                s3.Object(bucket, file_key).delete()
 
-            log.info(
-                (
-                    f"File {self.file_key} Successfully Removed from"
-                    f" {self.incoming_bucket_name}"
-                )
-            )
+            log.info((f"File {file_key} Successfully Removed from" f" {bucket}"))
 
         except botocore.exceptions.ClientError as e:
             log.error(e)
